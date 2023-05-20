a9b2ac461c580334d582beb57679f7f1b3a53d76
dungpham271095 <phamdvdung@gmail .com>
2023-05-20 15:17:14 +0700

    Restructure project

---
+++ core/commons/GlobalConstants.java
@@ -0,0 +1,5 @@
+package commons;
+
+public class GlobalConstants {
+	public static final String DEV_URL = "http://live.techpanda.org/";
+}
---
+++ core/factoryEnvirontment/EnvironmetFactory.java
@@ -0,0 +1,5 @@
+package factoryEnvirontment;
+
+public interface EnvironmetFactory {
+
+}
